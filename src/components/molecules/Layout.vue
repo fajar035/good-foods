bac42e8e75d330d25102aa1cf24d61ad39314e47
fajar035 <ikehikeh151@gmail.com>
2023-05-31 17:48:38 +0700

    setup layout project

---
+++ src/components/molecules/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar />
+  <slot/>
+  <Footer />
+</template>
+
+<script>
+import Navbar from '@/components/molecules/Navbar.vue'
+import Footer from '@/components/molecules/Footer.vue'
+
+export default {
+  name: 'LayoutPage',
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
---
+++ src/components/molecules/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="flex w-full h-auto justify-center align-center" >
+    <p class="text-3xl text-red-600">Navbar</p>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent'
+}
+</script>
